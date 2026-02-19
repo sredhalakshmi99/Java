af0c062fa55e6dadcb8eb1a66313d6512307ec46
sredha <sredhalakshmi99@gmail.com>
2026-02-19 19:51:41 +0530

    largest value

---
+++ Largest.java
@@ -0,0 +1,19 @@
+public class Largest
+{
+	public static void main(String[] arg)
+	{
+		int a=3,b=6,c=2;
+		if(a>=b && a>=c)
+		{
+			System.out.println("a is the largest");
+		}
+		else if(b>=a && b>=c)
+		{
+			System.out.println("b is the largest");
+		}
+		else
+		{
+			System.out.println("c is the largest");
+		}
+	}
+}
